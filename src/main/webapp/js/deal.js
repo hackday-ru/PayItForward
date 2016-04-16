067e5d566a4bc23c420d00190208f79dc588893c
Mikhail Koveshnikov <mike_35_92@mail.ru>
2016-04-16 21:17:58 +0300

    Added accept and reject angular service

--- src/main/webapp/js/deal.js
+++ src/main/webapp/js/deal.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('payForward.deal', ['ngRoute', 'ngResource','dealsServices'])
-    .controller('DealCtrl', ['$scope', 'IncomingDeals', 'OutgoingDeals', function ($scope, IncomingDeals, OutgoingDeals) {
+    .controller('DealCtrl', ['$scope', 'IncomingDeals', 'OutgoingDeals', 'AcceptDeal', 'RejectDeal',
+     function ($scope, IncomingDeals, OutgoingDeals, AcceptDeal, RejectDeal) {
         $scope.incomingDeals = IncomingDeals.query();
         $scope.outgoingDeals = OutgoingDeals.query();
+
+        AcceptDeal.save({dealId:123});
+        RejectDeal.save({dealId:321});
         console.log($scope)
     }]);
