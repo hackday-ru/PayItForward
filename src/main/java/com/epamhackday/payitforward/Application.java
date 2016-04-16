34471e1d4638622f2c2818755e3860573e22eff5
Aleksandr_Gruzdev <gralll@mail.ru>
2016-04-16 14:03:07 +0300

    -update build

--- src/main/java/com/epamhackday/payitforward/Application.java
+++ src/main/java/com/epamhackday/payitforward/Application.java
@@ -1,5 +1,10 @@
 package com.epamhackday.payitforward;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by bu3apd on 11/14/2014.
  */
