7600ce74b0d0203a09f320dd05bd7738f71e0a9d
Mikhail Koveshnikov <mike_35_92@mail.ru>
2016-04-16 19:18:16 +0300

    Added controllers and repositories for deals window

--- src/main/groovy/com/epamhackday/payitforward/repository/DealRepository.groovy
+++ src/main/groovy/com/epamhackday/payitforward/repository/DealRepository.groovy
@@ -5,4 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository
 
 interface DealRepository extends MongoRepository<Deal, Long> {
     List<Deal> findByInitiatorUserEmail(String userEmail)
+
+    List<Deal> findByInitiatorUserNameOrderByDate(String userName)
+
+    List<Deal> findByAcceptorUserNameOrderByDate(String userName)
 }
