9625d499c8da6f180c24111092cf35c04541cb9b
Mikhail Koveshnikov <mike_35_92@mail.ru>
2016-04-16 21:17:58 +0300

    Added accept and reject angular service

--- src/main/webapp/js/services.js
+++ src/main/webapp/js/services.js
@@ -9,3 +9,13 @@ dealsServices.factory('OutgoingDeals', ['$resource',
   function($resource){
     return $resource('/deal/outgoing');
   }]);
+
+dealsServices.factory('AcceptDeal', ['$resource',
+  function($resource){
+    return $resource('/deal/accept/:dealId', {dealId:'@dealId'});
+  }]);
+
+dealsServices.factory('RejectDeal', ['$resource',
+  function($resource){
+    return $resource('/deal/reject/:dealId', {dealId:'@dealId'});
+  }]);
