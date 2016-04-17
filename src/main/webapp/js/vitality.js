c9d700142316c28015dda2fecb3fe149073014e8
bu3apd bu3apd <bu3apd bu3apd>
2016-04-17 12:34:37 +0300

    awesome start page

---
+++ src/main/webapp/js/vitality.js
@@ -0,0 +1,60 @@
+/*!
+ * Vitality v1.3.5 (http://themes.startbootstrap.com/vitality-v1.3.5)
+ * Copyright 2013-2015 Start Bootstrap Themes
+ * To use this theme you must have a license purchased at WrapBootstrap (https://wrapbootstrap.com)
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth Scrolling: Smooth scrolls to an ID on the current page.
+    // To use this feature, add a link on your page that links to an ID, and add the .page-scroll class to the link itself. See the docs for more details.
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Activates floating label headings for the contact form.
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    
+   
+
+    
+
+    
+
+})(jQuery); // End of use strict
+
+
+
+// Analytics Tracking - DEMO ONLY!
+
+(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-38417733-23', 'auto');
+ga('send', 'pageview');
